6b38287f92b7de3d439e9ce0db37ae76c657b66b
Patgal0178 <pdgallant.pdg@gmail.com>
2021-02-25 19:29:15 -0400

    part3

--- ASN2/rickAndMortyQuiz/app/src/main/java/com/example/rickandmortyquiz/game/GameFragment.kt
+++ ASN2/rickAndMortyQuiz/app/src/main/java/com/example/rickandmortyquiz/game/GameFragment.kt
@@ -6,13 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.rickandmortyquiz.R
+import com.example.rickandmortyquiz.databinding.GameFragmentBinding
+
+private var score: Int = 0
 
 class GameFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GameFragment()
-    }
+
+    private lateinit var binding: GameFragmentBinding
 
     private lateinit var viewModel: GameViewModel
 
@@ -20,13 +28,25 @@ class GameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.game_fragment, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
+        )
+
+        binding.gameViewModel = viewModel
+
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameFragmentToGameOverFragment(score)
+        score = viewModel.score.value?:0
+        NavHostFragment.findNavController(this).navigate(action)
+        viewModel.onGameFinishedDone()
     }
 
 }
